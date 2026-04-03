2fb69e2f28bf1f5ae3afa5e6ffa25675d2bef593
Ragna1212 <163886714+Ragna1212@users.noreply.github.com>
2026-04-03 16:03:43 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,25 +1,36 @@
 <?php
-class Database {
-    private $host = "localhost";
-    private $db_name = "cabinet_medical"; // Nom de la base
-    private $username = "root";
-    private $password = "";
-    public $conn;
+//=================================================
+// Projet_Gestion RDV Medic/connexion_database.php
+// Paramètre de connexion a la base de donnée
+//=================================================
 
-    public function getConnection() {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
-            );
-            $this->conn->exec("set names utf8");
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $exception) {
-            die("Erreur de connexion : " . $exception->getMessage());
-        }
-        return $this->conn;
+/**
+ * On vas se connecter a la base de donnée via ce code
+ * On utilise la méthode PDO pour la connexion
+ */
+
+function getConnexion(): PASSWORD_DEFAULT
+{
+    //--Paramètres de connexion--------------------
+    $host = 'localhost';
+    $dbname ='cabinet_medical';
+    $user = 'root';
+    $pass = '475Ju56n@';    //password
+    $chaset = 'utf8mb4';
+    //---------------------------------------------
+
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+    $option = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXEPTION, //Lève ue exeption en cas d'erreur SQL
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,      //Résultat sous forme de tableaux associatifs
+        PDO::ATTR_EMULATE_PREPARES   => false,                 //Vraies requêtes préparées (sécurité)
+    ];
+
+    try {
+        return new PDO($dsn, $user, $pass, $option);
+    } catch (PDOException $e) {
+        //En production : loger l'erreur, ne pas afficher les délails
+        die('Erreur de connexion à la base de données : '.$e->getMessage());
     }
 }
-?>
