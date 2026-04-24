0cd4160a3f7731fff011ac73f466505afd034d71
MorganRidetLeRoy <nanouilleridetleroy86@gmail.com>
2026-04-24 15:24:01 +0200

    Add files via upload

---
+++ Controllers/controllersSecretaire.php
@@ -0,0 +1,17 @@
+<?pĥp 
+//  ______________________________________________________________________________________________________________
+// | Contrôller de la page de recherche d'agenda des médecins et les fiches des patients ainsi que l'ajoût de RDV |
+// | Reçois une requête http (GET/POST + paramètres)                                                              |
+// |                                                                                                              | 
+// | Contients les fonctions:                                                                                     |
+// |   - Recherches médecins                                                                                      |
+// |   - Ajoût de rendez vous (pour les médecins, donc modifications de leurs emploie du temps/agenda)            |
+// |______________________________________________________________________________________________________________|
+
+require_once __DIR__ . '/../Model/Medecin.php';
+
+
+class Secretaire
+{
+    
+}
