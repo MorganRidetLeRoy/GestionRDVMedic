0c32b1fb5efcfe4c7161efdf1b0f4a748a9575d4
MorganRidetLeRoy <nanouilleridetleroy86@gmail.com>
2026-04-24 16:31:42 +0200

    Update controllersSecretaire.php
    Ajoûts de la fonction recherche_medecin

--- Controllers/controllersSecretaire.php
+++ Controllers/controllersSecretaire.php
@@ -13,5 +13,29 @@ require_once __DIR__ . '/../Model/Medecin.php';
 
 class Secretaire
 {
+    public function recherche_medecin(string $terme): array|false
+    {
+    // On définit la clé (Idéalement, utilise une constante ou une variable d'environnement)
+    $key = 'Clé de Chiffrement78513';
+
+    $sql = "SELECT 
+                id_medecin, 
+                AES_DECRYPT(nom, :key) AS nom, 
+                AES_DECRYPT(prenom, :key) AS prenom, 
+                AES_DECRYPT(email_pro, :key) AS email_pro,
+                genre
+            FROM medecin
+            HAVING nom LIKE :terme 
+               OR prenom LIKE :terme 
+               OR email_pro LIKE :terme
+            ORDER BY nom ASC";
+
+    $query = $this->db->prepare($sql);
+    $query->execute([
+        'key'   => $key,
+        'terme' => '%' . $terme . '%'
+    ]);
     
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
